d90a44fa91be2e1697c41217d3f95aaa71d97e60
Mark Musil <musilmark25@gmail.com>
2024-01-20 13:24:59 -0500

    add var

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ var callAPI = (text, dest_language, setTranslation_output) =>{
     console.log(JSON.parse(result).body["TranslatedText"]);
     console.log(JSON.parse(result).body["TranslatedText"]);
     console.log(JSON.parse(result).body.TranslatedText);
-    translation_output = JSON.parse(result).TranslatedText;
+    var translation_output = JSON.parse(result).TranslatedText;
     console.log(translation_output);
     setTranslation_output(translation_output);
     })
